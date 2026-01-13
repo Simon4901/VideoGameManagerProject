7ba773caf2a22994b45963f703d24a3600c77444
AntiDote <sangin741205@gmail.com>
2026-01-13 01:19:52 +0100

    added preis in games but will do the rest tommorow

--- src/Games.java
+++ src/Games.java
@@ -16,32 +16,33 @@ public class Games {
 
     public boolean multiplayer;
 
-    public int price;
+    public int Preis;
 //Constructor
-    public Games(String Name, int USK, String Genre, double Metascore, int Spielerzahl, boolean Multiplayer) { //add price
+    public Games(String Name, int USK, String Genre, double Metascore, int Spielerzahl, boolean Multiplayer , int Preis) {
         this.Name = Name;
         this.USK = USK;
         this.Genre = Genre;
         this.Metascore = Metascore;
         this.Spieleranzahl = Spielerzahl;
         this.multiplayer = Multiplayer;
+        this.Preis= Preis;
     }
     //Creates 3 Objects and ads them to the List
-    public static void initObjects(){//add price
-        gameList.add(new Games("test", 18, "Genre", 10, 4, true));
-        gameList.add(new Games("Halo", 18, "Shooter", 8, 4, true));
-        gameList.add(new Games("Assasins Creed 2", 16, "Adventure", 10, 1, false));
+    public static void initObjects(){
+        gameList.add(new Games("test", 18, "Genre", 10, 4, true , 1));
+        gameList.add(new Games("Halo", 18, "Shooter", 8, 4, true, 10 ));
+        gameList.add(new Games("Assasins Creed 2", 16, "Adventure", 10, 1, false, 12));
     }
     //little Function to add something to the list, need more because it only fills the Name of the Game for now
-    public static void add (String eingabe, int Usk, String Genre, double Metascore, int Spielerzahl, boolean Multiplayer) {//add price
-        gameList.add(new Games(eingabe, Usk, Genre, Metascore, Spielerzahl, Multiplayer));
+    public static void add (String eingabe, int Usk, String Genre, double Metascore, int Spielerzahl, boolean Multiplayer, int Preis) {//add price
+        gameList.add(new Games(eingabe, Usk, Genre, Metascore, Spielerzahl, Multiplayer, Preis));
     }
 
     //DO NOT TOUCH, i dont know if we need it but maybe we do. it makes the whole list to a single String
     @Override
     public String toString() {
-        return Name + USK + Genre + Metascore + Spieleranzahl + multiplayer;
-    }//add price
+        return Name + USK + Genre + Metascore + Spieleranzahl + multiplayer + Preis;
+    }
 
 
 
